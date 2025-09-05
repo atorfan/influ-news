3b61b5fc66a5820009d642fc85352f757c9b46cb
alecs <alex.torfan@gmail.com>
2025-09-05 13:34:11 +0200

    feat: allow any origin to make http requests for dev

--- apps/image-rescale/src/main/java/com/newsnow/platform/imagerescale/infrastructure/configuration/RestApiConfiguration.java
+++ apps/image-rescale/src/main/java/com/newsnow/platform/imagerescale/infrastructure/configuration/RestApiConfiguration.java
@@ -1,9 +1,50 @@
 package com.newsnow.platform.imagerescale.infrastructure.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.newsnow.platform.imagerescale.adapters.driver.restapi")
-public class RestApiConfiguration {
+public class RestApiConfiguration implements WebMvcConfigurer {
+
+    @Value("${app.http.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+        ;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper()));
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var localDateModule = new SimpleModule();
+        localDateModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        var mapper = new ObjectMapper();
+        mapper.registerModules(localDateModule);
+        return mapper;
+    }
 }
